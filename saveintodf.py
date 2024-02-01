fe51b22ec7a422e5603a68d1117628f048bace25
B N V Sai Manoj <v-mbnvsai@microsoft.com>
2024-02-01 14:43:40 +0530

    Fixed the issue of using the default mail id, fixed the issue of sending mail with mulitple attachments, fixed the issue with exceding size limit for attachments

--- saveintodf.py
+++ saveintodf.py
@@ -4,6 +4,8 @@ import datetime
 import pandas as pd
 import win32com.client as win32
 import warnings
+import py7zr
+
 warnings.filterwarnings("ignore")
 
 def extract_formulas(file_path):
@@ -40,6 +42,7 @@ def extract_format(file_path):
     df = pd.DataFrame(formats_with_details, columns=['Path', 'Excel Name', 'Sheet Name', 'Coordinates', 'Format Details'])  # Update the column names
     return df
 
+countofformats = 0
 def compare_formats(df1,df2):
     global countofformats
     merged_df = pd.merge(df1, df2, on=['Sheet Name', 'Coordinates'], how='outer', suffixes=('_1', '_2'))
@@ -56,15 +59,26 @@ def compare_formulas(df1, df2):
     countofflase = len(merged_df[merged_df['Status'] == False])
     return merged_df
 
-def sendmail(recipients, file_path3, reportname=None, Location1=None, Location2=None, cc_recipients=None):
+def sendmail(recipients, file_path3, file_path4, reportname=None, Location1=None, Location2=None, cc_recipients=None):
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = recipients
-    mail.CC = cc_recipients
+    # mail.CC = cc_recipients
+
+    files_to_compress = ["formats_comparison.xlsx", "formulas_comparison.xlsx"]
+
+    with py7zr.SevenZipFile("Comparission_files.7z", "w") as archive:  # Faster, less compression
+        for filename in files_to_compress:
+            archive.write(filename)
+    for acc in outlook.Session.Accounts:
+        if str(acc) == 'saimanojb@maqsoftware.com':
+            reqmail = acc
+            break
+    mail._oleobj_.Invoke(*(64209, 0, 8, 0, reqmail))  # Set the sender account
     status = 'Success' if countofflase == 0 else 'Failure'
     mail.Subject = status + ' | ' + 'Excel Formula Comparison' + ' | ' + reportname
-    attachment3 = file_path3
-    mail.Attachments.Add(attachment3)
+    attachment = os.getcwd() + '\\Comparission_files.7z'
+    mail.Attachments.Add(attachment)
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     html_body = f'''
     <html>
@@ -75,6 +89,7 @@ def sendmail(recipients, file_path3, reportname=None, Location1=None, Location2=
         <p><strong>Location 2:</strong> {Location2}</p>
         <p><strong>Status:</strong> <span style="color: {'green' if status == 'Success' else 'red'}"><strong>{status}</strong></span></p>
         <p><strong>No of not matching formulas:</strong> {countofflase}</p>
+        <p><strong>No of not matching formats:</strong> {countofformats}</p>
         <p><strong>Executed time:</strong> {current_time}</p>
         <br>
         <p>Thanks and Regards,</p>
@@ -139,29 +154,29 @@ def compare_cformats(df1, df2):
     return merged_df
                 
 if __name__ == '__main__':
-    file_path1 = 'C:\\Users\\v-mbnvsai\\OneDrive - Microsoft\\Desktop\\Dev\\test11.xlsm'
-    file_path2 = 'C:\\Users\\v-mbnvsai\\OneDrive - Microsoft\\Desktop\\Prod\\test21.xlsm'
+    file_path1 = 'C:\\Users\\v-mbnvsai\\OneDrive - Microsoft\\Desktop\\Dev\\American Triangulation - January Forecast Dev.xlsm'
+    file_path2 = 'C:\\Users\\v-mbnvsai\\OneDrive - Microsoft\\Desktop\\Prod\\American Triangulation - January Forecast Prod.xlsm'
     reportname = os.path.basename(file_path1)
+
     Location1 = os.path.basename(os.path.dirname(file_path1))
     Location2 = os.path.basename(os.path.dirname(file_path2))
-    # df1 = extract_formulas(file_path1)
-    # df2 = extract_formulas(file_path2)
-    # merged_df = compare_formulas(df1, df2)
-    # merged_df.to_excel('formulas_comparison.xlsx', index=False)
+    df1 = extract_formulas(file_path1)
+    df2 = extract_formulas(file_path2)
+    merged_df = compare_formulas(df1, df2)
+    merged_df.to_excel('formulas_comparison.xlsx', index=False)
     file_path3 = 'C:\\Users\\v-mbnvsai\\OneDrive - Microsoft\\Documents\\Learning\\Py\\Formulas\\formulas_comparison.xlsx'
-    # recipients = 'saimanojb@maqsoftware.com'
-    # cc_recipients = 'saipavanm@maqsoftware.com'
-    # recipients = input("Enter recipients: ")
-    # cc_recipients = input("Enter cc recipients: ")
-    # sendmail(recipients, file_path3, reportname,Location1,Location2,cc_recipients)
+    # recipients = 'seshuc@maqsoftware.com'
+    # #cc_recipients = 'saipavanm@maqsoftware.com'
+    recipients = input("Enter recipients: ")
+    cc_recipients = input("Enter cc recipients: ")
 
-    # df3 = extract_format(file_path1)
-    # df4 = extract_format(file_path2)
-    # merged_df1 = compare_formats(df3, df4)
-    # merged_df1.to_excel('formats_comparison.xlsx', index=False)
-
-    df5 = conditional_formatting(file_path1)
-    df6 = conditional_formatting(file_path2)
-    merged_df2 = compare_cformats(df5, df6)
-    merged_df2.to_excel('cformats_comparison.xlsx', index=False)
-
+    df3 = extract_format(file_path1)
+    df4 = extract_format(file_path2)
+    merged_df1 = compare_formats(df3, df4)
+    merged_df1.to_excel('formats_comparison.xlsx', index=False)
+    file_path4 = 'C:\\Users\\v-mbnvsai\\OneDrive - Microsoft\\Documents\\Learning\\Py\\Formulas\\formats_comparison.xlsx'
+    # df5 = conditional_formatting(file_path1)
+    # df6 = conditional_formatting(file_path2)
+    # merged_df2 = compare_cformats(df5, df6)
+    # merged_df2.to_excel('cformats_comparison.xlsx', index=False)
+    sendmail(recipients, file_path3, file_path4, reportname, Location1, Location2, cc_recipients=None)
